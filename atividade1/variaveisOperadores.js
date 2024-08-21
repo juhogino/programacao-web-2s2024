e959848c97c9e7cf9e6e10e9adaca2016878ace3
juhogino <116019433+juhogino@users.noreply.github.com>
2024-08-21 10:37:24 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,16 @@
+let num_A1 = 10;
+let num_B1 = 5;
+
+let num_A2 = 20;
+let num_B2 = 4;
+
+let resultadoSoma = num_A1 + num_B1;
+let resultadoSubtracao = num_A2 - num_B2;
+let resultadoMultiplicacao = num_A1 * num_B1;
+let resultadoDivisao = num_A2 / num_B2;
+
+
+console.log(`Resultado da soma entre ${num_A1} e ${num_B1} é ${resultadoSoma}`);
+console.log(`Resultado da subtração entre ${num_A2} e ${num_B2} é ${resultadoSubtracao}`);
+console.log(`Resultado da multiplicação entre ${num_A1} e ${num_B1} é ${resultadoMultiplicacao}`);
+console.log(`Resultado da divisão entre ${num_A2} e ${num_B2} é ${resultadoDivisao}`);
