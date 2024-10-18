c40e5e887bc53e4a8fab4ad1c9d3f477e6e018ea
juhogino <116019433+juhogino@users.noreply.github.com>
2024-10-18 10:16:07 -0300

    Update index.js

--- app_valida_form/index.js
+++ app_valida_form/index.js
@@ -3,40 +3,41 @@ const mustacheExpress = require('mustache-express');
 const app = express();
 
 app.engine('html', mustacheExpress());
-app.set('view engine','html');
+app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.use(express.urlencoded({exteended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.render('index.html');
+app.get('/', (req, res) => {
+    let erro_form = req.query.erro_form;
+    let nome = req.query.nome;
+    let endereco = req.query.endereco;
+    let telefone = req.query.telefone;
+    let data_agendamento = req.query.data_agendamento;
+    res.render('index.html', { erro_form, nome, endereco, telefone, data_agendamento });
 });
 
-app.post('/dados', (req, res)=>{
-    let dados ={
-        nome:req.body.nome,
-        data_nascimento: req.body.data_nascimento,
-        descriao: req.body.descriao
+app.post('/dados', (req, res) => {
+    let dados = {
+        nome: req.body.nome,
+        endereco: req.body.endereco,
+        telefone: req.body.telefone,
+        data_agendamento: req.body.data_agendamento
     };
+
     let erro_form = false;
-    if(dados.nome.length == 0){
-        erro_form = true;
-    }
-    if(dados.data_nascimento.length == 0){
-        erro_form = true;
-    }
-    if(dados.descriao.length == 0){
+    if (!dados.nome || !dados.endereco || !dados.telefone || !dados.data_agendamento) {
         erro_form = true;
     }
 
-    if(erro_form){
-        res.redirect('/?erro_form=true');
+    if (erro_form) {
+        res.redirect(`/?erro_form=1&nome=${dados.nome}&endereco=${dados.endereco}&telefone=${dados.telefone}&data_agendamento=${dados.data_agendamento}`);
+    } else {
+        res.render('dados.html', { dados });
     }
-    else{
-    res.render('dados.html', {dados});
-}
 });
 
 const PORT = 8080;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log('app rodando na porta ' + PORT);
 });
+
